473e192a68fc95faf0977614c60280d4aa413190
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-01-26 01:53:36 +0500

    added models

---
+++ server/controllers/controllers.go
@@ -0,0 +1,32 @@
+package controllers;
+
+import (
+	"net/http"
+)
+
+// ADD NEW CONTROLLER!
+func InsertOnePost(w http.ResponseWriter, r *http.Request){
+// READ BODY !!
+
+// ADD DATA IN DATABASE!
+
+// RESPONSE BACK!
+}
+
+// EDIT CONTROLLER!
+
+
+
+// ADD LIKES!!
+
+
+
+// ADD DISLIKES!
+
+
+
+// REMOVE LIKE!
+
+
+
+// REMOVE DISLIKES!
--- server/models/post.model.go
+++ server/models/post.model.go
@@ -1,1 +1,11 @@
 package post;
+
+
+
+
+var Post struct {
+	ID          models.PrimitiveObjectID   `json:_id,omitempty`
+	Title       string                     `json:_id,omitempty`
+	Likes       int64                      `json:likes,omitempty`
+	dislikes    int64                      `json:dislikes,omitempty`
+}
